ab05857fef5a8e8e556e5e2f1becf0f09c6c2309
SumedhSankhe <sumedh.sankhe@gmail.com>
2020-01-13 14:31:31 -0500

    updated to ui, moving majority work from server.R to functions.R

---
+++ functions.R
@@ -0,0 +1,218 @@
+#### Utility Wrappers ####
+
+#' @title Show faults of an expression interactively
+#' @description Evaluates the given expression, function and its arguements
+#' return the output of the expression. If any errors/warnings are found a message
+#' is logged in the console. If used in a shiny App a notificatio is shown, for any
+#' errors process from the shiny app is stopped. Warning are shown and the shiny
+#' process continues as developed.
+#' @param ... Arguements passed to conditions, the expression to execure
+#' @param session A shiny object of shiny web app, defaults to NULL
+#' @return A return object of type as specified by the expression being executed
+#' @references https://github.com/SumedhSankhe/workBench/blob/fd5e808e86e849a933a924735d30348974212fc3/functions.R#L1-L37
+show_faults <- function(..., session = NULL){
+  warn <- err <- NULL
+  res <- withCallingHandlers(tryCatch(..., error = function(e) {
+    err <<- conditionMessage(e)
+    NULL
+  }), warning = function(w) {
+    warn <<- append(warn, conditionMessage(w))
+    invokeRestart("muffleWarning")
+  })
+  
+  if(!is.null(session) & !is.null(err)){
+    shiny::showNotification(as.character(err), duration = 20, type = 'error',
+                            session = session)
+    shiny::validate(shiny::need(is.null(err), as.character(err)))
+  } else if(is.null(session) & !is.null(err)) {
+    stop(Sys.time(),": ",err)
+  } else if (!is.null(session) & !is.null(warn)){
+    shiny::showNotification(as.character(err), duration = 20, type = 'Warning',
+                            session = session)
+  } else if(is.null(session) & !is.null(warn)) {
+    warning(Sys.time(),": ",warn)
+    return(res)
+  } else {
+    return(res)
+  }
+}
+
+
+#' @title Display Status messages to the console of the shiny-session
+#' @description A utility function which uses the native R and shiny reporting functions
+#' to display status of a particular process to the console or the shiny message
+#' bar with progress
+#' @param detail A character string of message to be displayed inthe console/shiny progress
+#' @param value A numeric value 0-1 which depicts the progress of the task at hand
+#' @param session A shiny session object
+#' @references https://github.com/SumedhSankhe/workBench/blob/fd5e808e86e849a933a924735d30348974212fc3/functions.R#L51
+status <- function(detail, value, session = NULL){
+  if(!is.null(session))
+    shiny::setProgress(value = value, message = "Progress:", detail = detail,
+                       session = session)
+  message(Sys.time(),": ",detail,"...")
+}
+
+#### Data Exploration ####
+
+explore_data <- function(format, count = NULL, annot = NULL, session = NULL){
+  
+  formatted_data <- show_faults(
+    format_data(format = format, count = count, annot = annot, session = session),
+    session = session
+  )
+  
+  plots <- show_faults(
+    generate_plots_explore(formatted_data = formatted_data, session = session),
+    session = session
+  )
+  
+  sum_table <- data.frame(Parameter =  c("Number of Proteins", "Number of Groups"),
+                          Values = c(formatted_data$n_prot,
+                                     formatted_data$n_group))
+  cond_sum_table <- show_faults(
+    expr = format_summary_table(data = formatted_data$long_data), session = session
+  )
+  data <- append(formatted_data, plots)
+  data <- append(data, list('sum_table' = sum_table, 'cond_sum_table' = cond_sum_table))
+  return(data)
+}
+
+#' @title Formats data to required longer format
+#' @description 
+#' @param  format A decided which data to use, user provided or default examples
+#' @param count A path to the data to be read in, any csv formats are acceptable
+#' @param annot An annotation file path as csv
+#' @param session A shiny session variable to make the notifications interactive
+#' @return A named list of the data an other objects as required
+format_data <- function(format, count = NULL, annot = NULL, session = NULL){
+  
+  shiny::validate(shiny::need(format %in% FORMATS, 'Undefined Format'))
+  
+  if(format == 'standard'){
+    status(detail = 'Importing Protein Abundance file', value = 0.4,
+           session = session)
+    wide <- fread(count, keepLeadingZeros = T)
+    
+    status(detail = 'Importing Annotations file', value = 0.5,
+           session = session)
+    annot <- fread(annot, keepLeadingZeros = T)
+  }else if(format == 'examples'){
+    status(detail = 'Importing Data from MSstatsSampleSize Package', value = 0.5,
+           session = session)
+    
+    wide <- as.data.table(MSstatsSampleSize::OV_SRM_train,
+                                      keep.rownames = T)
+    annot <- as.data.table(MSstatsSampleSize::OV_SRM_train_annotation)
+    setnames(wide,'rn','Protein')
+    name <- "Ovarian Cancer SRM study"
+  }else{
+    stop('Not Defined')
+  }
+  
+  status(detail = 'Stretching the data', value = 0.6, session = session)
+  data <- tidyr::gather(wide, 'BioReplicate', 'Abundance', 2:ncol(wide))
+  
+  status(detail = 'Merging Abundance & Spectral Counts with Annotations', 
+         value = 0.7, session = session)
+  data <- merge(as.data.table(data), annot, by = 'BioReplicate')
+  
+  if(!is.null(session)){
+    shiny::showNotification('Data Import Completed', duration = 5, 
+                            type = 'message')
+  }
+  
+  return(list('long_data' = data, 'wide_data' = wide, 'annot_data' = annot,
+              'n_prot' = nrow(wide[,.N, Protein]), 
+              'n_group' = nrow(annot[,.N,Condition]),
+              'is_imported' = T, 'dataset_name' = name))
+}
+
+
+generate_plots_explore <- function(formatted_data = NULL, session = NULL){
+  
+  shiny::validate(shiny::need(formatted_data, 'No Data Found to visualize'))
+  status(detail = 'Creating Boxplot for Protein Abundace', value = 0.8,
+         session = session)
+  
+  box_plot <- plotly::plot_ly(data = formatted_data$long_data[!is.na(Abundance)],
+                       y = ~Abundance, x = ~BioReplicate, color = ~Condition,
+                       type = "box") %>%
+    plotly::layout(xaxis = list(title="Biological Replicate"), 
+                   yaxis = list(title="Protein abundance"))
+  
+  status(detail = 'Estimating Variance', value = 0.85, session = session)
+  est_var <- MSstatsSampleSize::estimateVar(data = formatted_data$wide_data,
+                                            annot = formatted_data$annot_data)
+  
+  status(detail = 'Creating Mean/SD plots', value = 0.95, session = session)
+  meansd_plot <- meanSDplot(data = est_var)
+  
+  return(list('boxplot' = box_plot, 'meanSDplot' = meansd_plot))
+}
+
+
+format_summary_table <- function(data = NULL){
+  shiny::validate(need(data, 'No Data Provided'))
+  
+  msruns <- unique(data[,.(BioReplicate, Condition)]) # Placeholder
+  msruns <- xtabs(~Condition, data = msruns) # Placeholder
+  
+  biorep <- unique(data[,.(BioReplicate, Condition)])
+  biorep <- xtabs(~Condition, data = biorep)
+  
+  summary <- rbind(msruns, biorep)
+  rownames(summary) <- c("# of MS runs","# of Biological Replicates")
+  return(summary)
+}
+
+
+
+meanSDplot <- function (data, x.axis.size = 10, y.axis.size = 10, smoother_size = 1, 
+                        xlimUp = 30, ylimUp = 3){
+  
+  plotdata <- data.table(mean = as.vector(data$mu), sd = as.vector(data$sigma))
+  plot.lowess <- lowess(cbind(plotdata$mean, plotdata$sd))
+  plot.lowess <- data.table(x = plot.lowess$x, y = plot.lowess$y)
+  meansdplot <- ggplot(data = plotdata, aes(mean, sd)) +
+    stat_density2d(aes(fill = ..density..^0.25), geom = "tile", contour = FALSE,
+                   n = xlimUp * 10) +
+    scale_fill_continuous(low = "white", high = "#0072B2") +
+    geom_point(alpha = 0.02, shape = 20) + 
+    geom_line(data = plot.lowess, aes(x, y), color = "orange", size = smoother_size) +
+    labs(x = "Mean protein abundance per condition", 
+         y = "Standard deviation per condition") +
+    scale_y_continuous(expand = c(0,0), limits = c(0, ylimUp)) +
+    scale_x_continuous(expand = c(0,0), limits = c(0, xlimUp)) +
+    theme(panel.background = element_rect(fill = "white", colour = "black"),
+          panel.grid.major = element_line(colour = "black"),
+          panel.grid.minor = element_blank(),
+          strip.background = element_rect(fill = "gray95"),
+          strip.text.x = element_text(colour = c("#00B0F6"), size = 14),
+          axis.text.x = element_text(size = x.axis.size, colour = "black"),
+          axis.text.y = element_text(size = y.axis.size, colour = "black"), 
+          axis.ticks = element_line(colour = "black"), 
+          axis.title.x = element_text(size = x.axis.size + 5, vjust = -0.4),
+          axis.title.y = element_text(size = y.axis.size + 5, vjust = 0.3),
+          legend.position = "none")
+  return(meansdplot)
+}
+
+#### Data Simulation ###
+
+simulate_gird <- function(data = NULL, n_protein = 10, sample_inc = 20, n_sample = 10, protein){
+  
+  uniq_prots <- data$long_data[,unique(Protein)]
+  protein_desc <- 1/n_protein
+  m_prot <- floor(length(uniq_prots) * protein_desc)
+  protein_num <- seq.int(from = m_prot, to = length(uniq_prots), by = m_prot)
+  
+  n_groups <- length(data$annot_data[,unique(Condition)])
+  train_size <- seq.int(from = sample_inc, to = sample_inc * n_sample,
+                        length.out = n_sample)
+  train_size <- train_size * n_groups
+  
+  df <- as.data.table(expand.grid(train_size,protein_num))
+  df[, sim := Var1 * Var2]
+  return(df)
+}
--- ui.R
+++ ui.R
@@ -1,146 +1,78 @@
 ###############################################################
 ## MSstatsSampleSize Shiny app
 ###############################################################
-
-
-###############################################################
-## header
-
-header <- dashboardHeader(
-    title = "MSstatsSampleSize - Shiny",
-    titleWidth = 300
-    #tags$li(class = "dropdown",
-    #        tags$li(class = "dropdown", actionLink("debug_save", label="Save Environment"))
-    #)
-)
-
-
-###############################################################
-## sidebar
-
-side <- dashboardSidebar(
-    width = 300,
-    sidebarMenu(id="tabs",
-                menuItem("Home", tabName="home", icon = icon("home")),
-                menuItem("Import Data",
-                         tabName="import_data", icon = icon("file-import")),
-                ## Use of conditionalPanel in place of subMenuItem, ref. https://github.com/rstudio/shinydashboard/issues/86
-                conditionalPanel("input.tabs === 'import_data'", 
-                         #startExpanded=TRUE,
-                         div(class="custom-sidebar-output",
-                             selectInput("data_format", "Select data type", 
-                                         choices=list("Protein-level quantification"="standard", 
-                                                      "Example from MSstatsSampleSize"="examples"))),
-                         htmlOutput("select_files", class="custom-sidebar-output",)),
-                menuItem("Simulate datasets", 
-                        tabName="explore_simulated", icon=icon("project-diagram")),
-                menuItem("Analyze the simulated datasets", 
-                        tabName="analyse_simulated", icon=icon("vial"))
-   )
-)
-
-
-###############################################################
-## body
-
-body <- dashboardBody(
-    useShinyjs(),
-    tags$head(
-        tags$style(HTML("
-            .box {
-                overflow-y: scroll;
-            }
-            
-            /* Styling for elements nested inside menuItems */
-            .treeview-menu .shiny-input-container {
-                margin-bottom:0px !important;
-                padding-top:0px !important;
-                padding-bottom:0px !important;
-            }
-            
-            .custom-sidebar-output>.shiny-input-container .progress{
-                margin-bottom:0px !important;
-            }
-            
-            .custom-sidebar-output>b {
-                padding-left:15px;
-            }
-            
-            .custom-sidebar-output {
-                margin: 10px 0 10px 0;
-            }
-            
-            .treeview-menu {
-                padding:5px 0px 5px 5px !important;
-            }
-
-            #main_output {
-                font-family: Monaco, Consolas, 'Andale Mono', 'DejaVu Sans Mono', monospace;
-                font-size: 80%;
-                margin-top: 5px;
-                white-space: pre;
-                white-space: pre-wrap;
-                white-space: -moz-pre-wrap;
-                white-space: -o-pre-wrap;
-                background: #222D32;
-                color: #FFFFFF;
-                border-radius: 2px;
-            }
-      
-            .content-wrapper, .right-side {
-                background-color: #F7F7F7;
-            }
-      
-            h1 {
-                margin-top: 0px;
-            }
-      
-            * {
-                word-break: break-word;
-            }
-            
-            .custom-box-title {
-                display: inline-block;
-                font-size: 18px;
-                margin: 0;
-                line-height: 1;
-            }
-            ")
-        )
-    ),
-    
-    tabItems(
-        Tab1 <- tabItem(
-            tabName="home",
-            includeMarkdown("www/Welcome.md")
-        ),
-
-        Tab2 <- tabItem(
-            tabName="import_data",
-            htmlOutput("import_data_content")
-        ),
-
-        Tab3 <- tabItem(
-            tabName="explore_simulated",
-            h1("Simulate datasets"),
-            htmlOutput("explore_simulated_content")
-        ),
-    
-        Tab4 <- tabItem(
-            tabName = "analyse_simulated",
-            h1("Analyze the simulated datasets"),
-            htmlOutput("analyse_simulated_content")
-        )
-    )
-)
-
-
-###############################################################
-## Finally, boad
-
 dashboardPage(
     skin="black",
-    header,
-    side,
-    body
+    ##### Header #####
+    header =  dashboardHeader(
+      title = "MSstatsSampleSize - Shiny",
+      titleWidth = 300
+    ),
+    #### SideBar ####
+    sidebar =  dashboardSidebar(
+      shinyjs::useShinyjs(),
+      width = 300,
+      sidebarMenu(
+        id="tabs",
+        ##### Home Tab ####
+        menuItem("Home", tabName = "home", icon = icon("home")),
+        #### Data Import Tab ####
+        menuItem("Import Data", tabName = "import_data",
+                 icon = icon("file-import"),
+                 ##### Select Input for Type of Data ####
+                 selectInput("data_format", "Select Data Type",
+                             choice = list("Protein-level quantification" = "standard", 
+                                           "Example from MSstatsSampleSize" = "examples")),
+                 ##### File Input for Protein Abundance File ####
+                 fileInput("standard_count", "Select Protein Abundance File",
+                           multiple = FALSE,
+                           accept = c("text/csv",
+                                      "text/comma-separated-values,text/plain",
+                                      ".csv", "text/tab-separated-values", ".tsv")),
+                 ##### File Input for Annotation File #####
+                 fileInput("standard_annot", "Select sample annotation file",
+                           multiple = FALSE,
+                           accept = c("text/csv",
+                                      "text/comma-separated-values,text/plain",
+                                      ".csv", "text/tab-separated-values", ".tsv")),
+                 ##### Action Button To load data #####
+                 actionButton("import_data", "Import dataset", 
+                              icon = icon("file-import"))
+        ),
+        #### Data Simulation Tab ####
+        menuItem("Simulate datasets", 
+                 tabName = "explore_simulated", icon = icon("project-diagram")),
+        #### Analyze Simulation Tab ####
+        menuItem("Analyze the simulated datasets", 
+                 tabName = "analyse_simulated", icon = icon("vial"))
+      )
+    ),
+    ##### Body ####
+    body = dashboardBody(
+      shinyjs::useShinyjs(),
+      tabItems(
+        #### Home Tab ####
+        tabItem(
+          tabName = "home",
+          includeMarkdown("www/Welcome.md")
+        ),
+        #### Import Data Tab ####
+        tabItem(
+          tabName = "import_data", class = "active",
+          htmlOutput("explore_data")
+        ),
+        #### Data Simulation Tab #####
+        tabItem(
+          tabName = "explore_simulated",
+          h1("Simulate datasets"),
+          htmlOutput("explore_simulated_content")
+        ),
+        #### Analyse Simulation Tab ####
+        tabItem(
+          tabName = "analyse_simulated",
+          h1("Analyze the simulated datasets"),
+          htmlOutput("analyse_simulated_content")
+        )
+      )
+    )
 )
